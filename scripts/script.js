a58e4200d8fcb47c40adf5a58e15a642d09fb3f2
Isadora <isadorameloofc@gmail.com>
2021-02-21 13:50:14 -0300

    versão atualizada

---
+++ scripts/script.js
@@ -0,0 +1,18 @@
+function enviaformulario(evento){
+	/*alert(evento.target);
+	evento.preventDefault();
+	console.log (evento.target);
+	console.log ("Cheguei na função");*/
+	let nome=document.getElementById("nome").value;
+	alert(nome);
+}
+{
+
+	let idade=document.getElementById("idade").value;
+	alert(idade);
+}
+	let email=document.getElementById("email").value;
+	alert(email);
+
+	let senha=document.getElementById("senha").value;
+	alert(senha);
