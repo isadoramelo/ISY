5493ee88fd4f97adf040927a33992e8f4f4ea032
isadora melo <isadorameloofc@gmail.com>
2021-07-24 11:59:04 -0300

    versão atualizada

--- scripts/carregadepoimentos.js
+++ scripts/carregadepoimentos.js
@@ -1,13 +1,13 @@
 function carregaDepoimentos() {
-  let db = firebase.firestore();
+	let db = firebase.firestore();
   //var database = firebase.database();
   let html = '';
   db.collection('depoimentos')
-    .get()
-    .then(querySnapshot => {
-      querySnapshot.forEach(doc => {
+  .get()
+  .then(querySnapshot => {
+  	querySnapshot.forEach(doc => {
         // console.log(`${doc.id} => ${doc.data()}`);
-        console.log(doc.id);
+        console.log(doc.data());
         console.log(doc.data().depoimento);
         var div = document.createElement('div');
         var row = document.createElement('div');
@@ -17,6 +17,7 @@ function carregaDepoimentos() {
         var span = document.createElement('span');
         var icon = document.createElement('i');
         var linha = document.createElement('br');
+        let likes = document.createElement('span');
         contador++;
 
         //Add classes
@@ -33,6 +34,9 @@ function carregaDepoimentos() {
         row.style.padding = '1rem 1rem';
         icon.style.fontSize = '36px';
         icon.style.color = `#${hex}`;
+        likes.style.fontSize = '12px';
+        likes.style.fontWeight = '600';
+        likes.id = `likes${doc.data().depoimento}`; // criando id especial em cada span do like para atualizar na hora q o usuário curtir
 
         col1.style.backgroundColor = 'white';
         col2.style.backgroundColor = '#DCDCDC';
@@ -63,8 +67,11 @@ function carregaDepoimentos() {
         image.style.width = '30px';
         image.style.cursor = 'pointer';
         let id = doc.id;
-        image.setAttribute("onclick", `curtir('${id}')`);
+        image.setAttribute('onclick', `curtir('${id}')`);
+
+        likes.append(doc.data().curtir);
         col3.append(image);
+        col3.append(likes);
         document.getElementById('depoimentos').appendChild(linha);
         //<div>
         //<row>
@@ -78,31 +85,36 @@ function carregaDepoimentos() {
         //<div/>
         //<br>
         // console.log(contador);
-      });
     });
+  });
 }
 
 function curtir(id) {
-  let db = firebase.firestore();
-  let docRef = db.collection("depoimentos").doc(id);
+	let db = firebase.firestore();
+	let docRef = db.collection('depoimentos').doc(id);
 
-  //Rebeca agora parece que está funcionando. 
+  //Rebeca agora parece que está funcionando.
   /* Deixa eu explicar o que está acontecendo aqui. Primeiro eu faço a leitura do 
   número de curtir que o depoimento já teve. Depois eu somo um nesse valor e somente após isso armazeno novamente no banco. 
   Agora cada depoimento, no banco de dados, tem o número de curtidas.
   Seria legal vocẽ tentar colocar esse número na tela agora. 
   */
-  docRef.get().then((doc) => {
-    if (doc.exists) {
-      let curtir = doc.data().curtir + 1;
-       // Atualizar curtir
-      db.collection("depoimentos").doc(id).update({ curtir: curtir });
+  docRef
+  .get()
+  .then(doc => {
+  	if (doc.exists) {
+  		let curtir = doc.data().curtir + 1;
+        // Atualizar curtir
+        db.collection('depoimentos').doc(id).update({ curtir: curtir });
+        // incrementando na página a atualização do número, no id dos likes.
+        document.getElementById(`likes${doc.data().depoimento}`).innerText =
+        curtir;
+        
     } else {
-      console.log("Depoimento não encontrado!");
+    	console.log('Depoimento não encontrado!');
     }
-  }).catch((error) => {
-    console.log("Erro para recuperar os dados do banco de dados:", error);
+})
+  .catch(error => {
+  	console.log('Erro para recuperar os dados do banco de dados:', error);
   });
-
-
 }
